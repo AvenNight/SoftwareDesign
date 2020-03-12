d70968ba0bc3eff98fc14678746c96dd7065d7ff
AvenNight <art-ibr@yandex.ru>
2020-03-12 15:44:28 +0700

    Reflection Practice init

---
+++ Reflection.Differentiation.csproj/Algebra.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Reflection.Differentiation
+{
+    public class Algebra
+    {
+        public static Expression Differentiate(Expression body)
+        {
+            if (body is ConstantExpression)
+                return Expression.Constant(0d);
+            else if (body is ParameterExpression)
+                return Expression.Constant(1d);
+            else if (body is BinaryExpression op)
+            {
+                if (body.NodeType is ExpressionType.Add)
+                    return Expression.Add(
+                        Differentiate(op.Left), Differentiate(op.Right)
+                        );
+                else if (body.NodeType is ExpressionType.Multiply)
+                    return Expression.Add(
+                        Expression.Multiply(Differentiate(op.Left), op.Right),
+                        Expression.Multiply(op.Left, Differentiate(op.Right))
+                        );
+            }
+            else if (body is MethodCallExpression)
+            {
+                var method = body as MethodCallExpression;
+                var arg = method.Arguments[0];
+                var methodName = method.Method.Name;
+                var diffRevert = body;
+                
+                if (methodName == "Sin")
+                    diffRevert = Expression.Call(typeof(Math).GetMethod("Cos"), arg);
+                else if (methodName == "Cos")
+                    diffRevert = Expression.Negate(
+                        Expression.Call(typeof(Math).GetMethod("Sin"), arg));
+                return Expression.Multiply(diffRevert, Differentiate(arg));
+            }
+            throw new NotImplementedException("Other diffs not implemented");
+        }
+
+        public static Expression<Func<double, double>> Differentiate(Expression<Func<double, double>> function) =>
+            Expression.Lambda<Func<double, double>>(Differentiate(function.Body), function.Parameters);
+    }
+}
---
+++ Reflection.Randomness.csproj/Generator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Reflection.Randomness
+{
+    public class FromDistributionAttribute : Attribute
+    {
+        private readonly Type type;
+        private readonly object[] @params;
+
+        public IContinousDistribution Distribution
+        {
+            get
+            {
+                if (@params.Length > 2) throw new ArgumentException($"{type}");
+                return (IContinousDistribution)Activator.CreateInstance(type, @params);
+            }
+        }
+
+        public FromDistributionAttribute(Type type, params object[] ps)
+        {
+            this.type = type;
+            @params = ps;
+        }
+    }
+
+    public class Generator<T> where T : new()
+    {
+        public readonly static Dictionary<PropertyInfo, IContinousDistribution> Distributions;
+        private static IEnumerable<PropertyInfo> properties;
+
+        static Generator()
+        {
+            properties = typeof(T).GetProperties();
+            Distributions = new Dictionary<PropertyInfo, IContinousDistribution>();
+        }
+
+        public T Generate(Random rnd)
+        {
+            T e = new T();
+            foreach (var prop in properties)
+            {
+                var curAttr = (FromDistributionAttribute)prop.GetCustomAttribute(typeof(FromDistributionAttribute));
+                IContinousDistribution rndDistrib = null;
+                if (Distributions.ContainsKey(prop))
+                    rndDistrib = Distributions[prop];
+                else if (curAttr != null)
+                    rndDistrib = curAttr.Distribution;
+                else
+                    continue;
+                prop.SetValue(e, rndDistrib.Generate(rnd));
+            }
+            return e;
+        }
+    }
+
+    public static class GeneratorExtensions
+    {
+        public static CustomGenerator<T> For<T>(this Generator<T> generator, Expression<Func<T, double>> pointer) where T : new()
+        {
+            if (pointer.Body is MemberExpression memberPointer)
+            {
+                var pointedProperty = (PropertyInfo)memberPointer.Member;
+                if (!typeof(T).GetProperties().Contains(pointedProperty)) throw new ArgumentException();
+                return new CustomGenerator<T>(generator, pointedProperty);
+            }
+            throw new ArgumentException();
+        }
+
+        public static Generator<T> Set<T>(this CustomGenerator<T> customGenerator, IContinousDistribution newDistribution) where T : new()
+        {
+            Generator<T>.Distributions[customGenerator.PointedProperty] = newDistribution;
+            return customGenerator.Generator;
+        }
+
+        public class CustomGenerator<T> where T : new()
+        {
+            public readonly Generator<T> Generator;
+            public readonly PropertyInfo PointedProperty;
+
+            public CustomGenerator(Generator<T> generator, PropertyInfo pointedProperty)
+            {
+                Generator = generator;
+                PointedProperty = pointedProperty;
+            }
+        }
+    }
+}
